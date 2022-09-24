2741957cf0e7d3aa6047e9b8e2c8393f395e7ec7
tHyt-lab <washers.fezzes-0q@icloud.com>
2022-09-24 12:51:05 +0900

    update script.

---
+++ README.md
@@ -0,0 +1,13 @@
+# 構成管理
+
+## スクリプトの実行
+当リポジトリをClone  
+`git clone https://github.com/tHyt-lab/dotfiles.git`
+
+クローンできればフォルダに移動してスクリプトを実行  
+`cd dotfiles`  
+`./setup.sh` or `make`
+
+## 注意点
+AppStoreからインストールするものは手動になります。(GitHubActionsでこけてしまうため)  
+.Brewfileにmasを追加して再度バンドルするのもあり。
--- setup.sh
+++ setup.sh
@@ -1,18 +1,53 @@
 #!/bin/bash
 
 # Set initial preference.
+cat << EOF
+
+##################################################
+  Set initial preference.
+##################################################
+
+EOF
 ./.bin/init.sh
 
 # Link dotfiles.
+cat << EOF
+
+##################################################
+  Link dotfiles.
+##################################################
+
+EOF
 ./.bin/link.sh
 
 # Install macOS applications.
+cat << EOF
+
+##################################################
+  Install macOS applications.
+##################################################
+
+EOF
 ./.bin/brew.sh
 
 # Set macOS system preferences.
+cat << EOF
+
+##################################################
+  Set macOS system preferences.
+##################################################
+
+EOF
 ./.bin/defaults.sh
 
 # Install Fonts
+cat << EOF
+
+##################################################
+  Install Fonts
+##################################################
+
+EOF
 ./.bin/font-install.sh
 
 exec $SHELL -l
