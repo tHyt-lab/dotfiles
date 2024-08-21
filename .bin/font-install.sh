d29f1abaeff03202e42eb4f71dba4ff2ebdc212a
tHyt-lab <washers.fezzes-0q@icloud.com>
2024-08-21 10:52:45 +0900

    chore: Update font installation script with UDEVGothic font

--- .bin/font-install.sh
+++ .bin/font-install.sh
@@ -7,9 +7,9 @@ unzip -d ./tmp/fonts ./tmp/2.012R.zip
 cp ./tmp/fonts/source-han-code-jp-2.012R/OTF/* ~/Library/Fonts/
 
 # UDEVGothic
-wget -O ./tmp/UDEVGothic_NF.zip https://github.com/yuru7/udev-gothic/releases/download/v1.3.1/UDEVGothic_NF_v1.3.1.zip
+wget -O ./tmp/UDEVGothic_NF.zip https://github.com/yuru7/udev-gothic/releases/download/v2.0.0/UDEVGothic_NF_v2.0.0.zip
 unzip -d ./tmp/fonts ./tmp/UDEVGothic_NF.zip
-cp ./tmp/fonts/UDEVGothic_NF_v1.3.1/UDEVGothicNF-*.ttf ~/Library/Fonts/
-cp ./tmp/fonts/UDEVGothic_NF_v1.3.1/UDEVGothic35NF-*.ttf ~/Library/Fonts/
+cp ./tmp/fonts/UDEVGothic_NF_v2.0.0/UDEVGothicNF-*.ttf ~/Library/Fonts/
+cp ./tmp/fonts/UDEVGothic_NF_v2.0.0/UDEVGothic35NF-*.ttf ~/Library/Fonts/
 
 rm -rf ./tmp
